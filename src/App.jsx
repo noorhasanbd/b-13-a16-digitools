2bc3e2606c2621756abe53ef86a90070c9d459eb
noorhasanbd <noor.hasan982845@gmail.com>
2026-03-30 08:56:52 +0600

    initialized the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="bg-amber-500">Hello Using Roboto</h2>
+    </>
+  );
+}
+
+export default App;
